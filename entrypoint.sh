8d9dab27e622e24eae9daa7ff46019294c4bd80e
Timo Metsälä <timo.metsala@reaktor.com>
2020-07-02 13:56:20 +0300

    Add override possibility to http port, too

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,16 +7,17 @@ DISKFREE=10
 INDEXBASE=
 PACKETSBASE=
 HTTP_HOST=127.0.0.1
+HTTP_PORT=9200
 
 set -euo pipefail
 
 usage() {
-    echo "Usage: [--host <http_host>] [--threads 1] [--filter <bpf>] [--diskfree 10] --interface <dev> --indexbase <dir> --packetsbase <dir>" >&2
+    echo "Usage: [--host <http_host>] [--port <http_port>] [--threads 1] [--filter <bpf>] [--diskfree 10] --interface <dev> --indexbase <dir> --packetsbase <dir>" >&2
     exit 1
 }
 
 parse_opts() {
-    local -r OPTS=$(getopt --options '' --longoptions host:,threads:,interface:,filter:,diskfree:,indexbase:,packetsbase: -n 'entrypoint' -- "$@")
+    local -r OPTS=$(getopt --options '' --longoptions host:,port:,threads:,interface:,filter:,diskfree:,indexbase:,packetsbase: -n 'entrypoint' -- "$@")
 
     if [ $? != 0 ]; then
         echo "Failed parsing options." >&2
@@ -62,6 +63,11 @@ parse_opts() {
             shift
             shift
             ;;
+        --port)
+            HTTP_PORT="$2"
+            shift
+            shift
+            ;;
         -h | --help)
             usage
             shift
@@ -83,6 +89,7 @@ create_config() {
     local -r DISKFREE="$5"
     local -r FILTER="$6"
     local -r HTTP_HOST="$7"
+    local -r HTTP_PORT="$8"
     local COMPILED_FILTER=
 
     if [ -n "${FILTER}" ]; then
@@ -101,7 +108,7 @@ create_config() {
     StenotypePath: "/opt/stenographer/bin/stenotype",
     Interface: $interface,
     Host: $http_host,
-    Port: 1234,
+    Port: $http_port,
     Flags: ([ "--seccomp=none", "--uid=root", "--gid=root" ] + $extraflags),
     CertPath: "/etc/stenographer/certs"
 }
@@ -116,6 +123,7 @@ EOF
     jq --null-input \
         --arg interface "${INTERFACE}" \
         --arg http_host "${HTTP_HOST}" \
+        --argjson http_port "${HTTP_PORT}" \
         --argjson threads "${THREADS}" \
         --arg packetsbase "${PACKETSBASE}" \
         --arg indexbase "${INDEXBASE}" \
@@ -127,9 +135,9 @@ EOF
 
 parse_opts "$@"
 
-if [ -z "${HTTP_HOST}" ] || [ -z "${INTERFACE}" ] || [ -z "${PACKETSBASE}" ] || [ -z "${INDEXBASE}" ] || [ "${THREADS}" -le 0 ] || [ "${DISKFREE}" -le 0 ]; then
+if [ -z "${HTTP_HOST}" ] || [ -z "${HTTP_PORT}" ] || [ -z "${INTERFACE}" ] || [ -z "${PACKETSBASE}" ] || [ -z "${INDEXBASE}" ] || [ "${THREADS}" -le 0 ] || [ "${DISKFREE}" -le 0 ]; then
     usage
 fi
 
-create_config "${INTERFACE}" "${THREADS}" "${PACKETSBASE}" "${INDEXBASE}" "${DISKFREE}" "${FILTER}" "${HTTP_HOST}" >/etc/stenographer/config
+create_config "${INTERFACE}" "${THREADS}" "${PACKETSBASE}" "${INDEXBASE}" "${DISKFREE}" "${FILTER}" "${HTTP_HOST}" "${HTTP_PORT}" >/etc/stenographer/config
 /opt/stenographer/bin/stenographer -syslog=false -config=/etc/stenographer/config
